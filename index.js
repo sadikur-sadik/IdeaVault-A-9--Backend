462b465636e198b1ae7b445aa93e244af10a6351
Sadikur Rahman <sadikursadikorg7@gmail.com>
2026-05-23 13:50:52 +0600

    CRUD: POST (Posting ideas)

--- index.js
+++ index.js
@@ -1,7 +1,53 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const envConfig = require('dotenv')
+envConfig.config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000
+const cors = require('cors');
 
+
+
+app.use(express.json())
+app.use(cors());
+
+
+const uri = process.env.MONGO_URI
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run () {
+  try {
+
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    const db = client.db("idea-vault-database")
+    const ideaDatabase = db.collection("ideas")
+    const commentDatabase = db.collection("comments")
+
+    // posting data
+
+    app.post('/ideas', async (req, res) => {
+      const newUser = req.body;
+    
+      const result = await ideaDatabase.insertOne(newUser);
+      res.send(result);
+    });
+
+    // posting comments
+ 
+  } finally {
+
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
